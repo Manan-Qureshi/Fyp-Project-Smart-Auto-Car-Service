09b049b98e83e10153ec653060f34b64a772360e
Muhammad Ramzan Jamshaid <jamshaidali8u@gmail.com>
2026-08-26 07:17:59 +0500

    add storage link to docker entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,6 +5,9 @@ php artisan config:cache
 php artisan route:cache
 php artisan view:cache
 
+# Create storage symlink
+php artisan storage:link --force
+
 # Run database migrations and seeders
 php artisan migrate --force
 php artisan db:seed --force
